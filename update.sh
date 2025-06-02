94a6c7c5a132fdf5cc20b1d2f57030fd7f09d4c4
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-06-02 08:49:23 +0200

    Fix commit informations

--- update.sh
+++ update.sh
@@ -22,8 +22,8 @@ elif [ $EXIT_CODE -eq 0 ]; then
 
     echo "This means that we now have changes we should push"
 
-    git config --global user.name 'github-actions'
-    git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
+    git config --global user.name 'MaximilienNaveau'
+    git config --global user.email 'maximilien.naveau@gmail.com'
 
     git add .
     git commit -m"Automated update"
